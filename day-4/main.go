e24e0d5d224381d790e0e0c3070b5914be31a1b5
kaynec <once462@gmail.com>
2024-12-07 01:50:44 +0330

    advent of code

--- day-4/main.go
+++ day-4/main.go
@@ -3,48 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
-
-func checkIsBound(slice [][]string, nums []int) bool {
-
-	fmt.Println(nums)
-
-	return false
-}
-
-func checkXmas(slice [][]string, row int, col int) {
-
-	numsSlice := make([][]int, 0)
-
-	numsSlice = append(numsSlice, []int{row, col}, []int{row, col + 1})
-	fmt.Println(numsSlice)
-
-	// checkIsBound(slice, &{[0,0]} )
-
-	// SAMX
-	// if strings.Join(slice[row], "")[col-4:col] == "SAMX" {
-	// 	fmt.Println("1")
-	// }
-	// // XMAS
-	// if strings.Join(slice[row], "")[col:col+4] == "XMAS" {
-	// 	fmt.Println("1")
-	// }
-	// if row+3 > len(slice) {
-	// 	return
-	// }
-	// fmt.Println(
-	// 	checkIsBound(slice, row, col+3),
-	// checkIsBound(slice, row, col-3),
-	// checkAngle(slice, row+3, col),
-	// checkAngle(slice, row-3, col),
-
-	// checkAngle(slice, row-3, col-3),
-	// checkAngle(slice, row-3, col-3),
-
-	// checkAngle(slice, row+3, col+3),
-	// checkAngle(slice, row+3, col+3),
-}
 
 func parseStr(path string) [][]string {
 	val, _ := os.ReadFile(fmt.Sprintf("%s.txt", path))
@@ -62,7 +23,48 @@ func parseStr(path string) [][]string {
 	return sliced
 }
 
-func partOneAnswer(path string) {
+func checkAnswer(slice [][]string, nums [][]int, answer string, shouldSort bool) bool {
+
+	s := ""
+	for _, num := range nums {
+		row, col := num[0], num[1]
+
+		if row >= len(slice) || row < 0 || col >= len(slice[0]) || col < 0 {
+			return false
+		}
+		s += slice[row][col]
+	}
+
+	if !shouldSort {
+		return s == answer
+	}
+
+	newS := strings.Split(s, "")
+
+	slices.Sort(newS)
+
+	newAnswer := strings.Split(answer, "")
+
+	slices.Sort(newAnswer)
+
+	return strings.Join(newS, "") == strings.Join(newAnswer, "")
+}
+
+func checkXmas(slice [][]string, row int, col int, numSlice [][][]int, answer string, shouldSort bool) int {
+
+	sum := 0
+
+	for _, matrix := range numSlice {
+
+		if checkAnswer(slice, matrix, answer, shouldSort) {
+			sum++
+		}
+	}
+
+	return sum
+}
+
+func partOneAnswer(path string) int {
 	mulSlice := parseStr(path)
 
 	sum := 0
@@ -71,44 +73,58 @@ func partOneAnswer(path string) {
 
 		for col := range mulSlice[row] {
 
+			templateSlice := [][][]int{
+				[][]int{[]int{row, col}, []int{row, col + 1}, []int{row, col + 2}, []int{row, col + 3}},
+				[][]int{[]int{row, col}, []int{row, col - 1}, []int{row, col - 2}, []int{row, col - 3}},
+				[][]int{[]int{row, col}, []int{row - 1, col}, []int{row - 2, col}, []int{row - 3, col}},
+				[][]int{[]int{row, col}, []int{row + 1, col}, []int{row + 2, col}, []int{row + 3, col}},
+				[][]int{[]int{row, col}, []int{row - 1, col - 1}, []int{row - 2, col - 2}, []int{row - 3, col - 3}},
+				[][]int{[]int{row, col}, []int{row - 1, col + 1}, []int{row - 2, col + 2}, []int{row - 3, col + 3}},
+				[][]int{[]int{row, col}, []int{row + 1, col - 1}, []int{row + 2, col - 2}, []int{row + 3, col - 3}},
+				[][]int{[]int{row, col}, []int{row + 1, col + 1}, []int{row + 2, col + 2}, []int{row + 3, col + 3}},
+			}
+
 			if mulSlice[row][col] == "X" {
-				checkXmas(mulSlice, row, col)
+				sum += checkXmas(mulSlice, row, col, templateSlice, "XMAS", false)
 			}
 
 		}
 	}
 
-	fmt.Println(sum)
+	fmt.Println("SUM", sum)
+	return sum
 
 }
 
-// func partTwoAnswer(path string) int {
-// 	mulSlice := parseStr(path, `(mul\(\s*-?\d+\s*,\s*-?\d+\s*\)|don't\(\)|do\(\))`)
+func partTwoAnswer(path string) {
+	mulSlice := parseStr(path)
 
-// 	shouldMul := true
+	sum, masTreshhold := 0, 2
 
-// 	sum := 0
+	for row := range mulSlice {
 
-// 	for _, value := range mulSlice {
+		for col := range mulSlice[row] {
+			if mulSlice[row][col] != "A" {
+				continue
+			}
 
-// 		if value == "do()" {
-// 			shouldMul = true
-// 			continue
-// 		}
-// 		if value == "don't()" {
-// 			shouldMul = false
-// 			continue
-// 		}
+			templateSlice := [][][]int{
+				[][]int{[]int{row, col}, []int{row - 1, col - 1}, []int{row + 1, col + 1}},
+				[][]int{[]int{row, col}, []int{row - 1, col + 1}, []int{row + 1, col - 1}},
+			}
 
-// 		if shouldMul {
-// 			sum += returnMul(value)
-// 		}
+			if checkXmas(mulSlice, row, col, templateSlice, "MAS", true) == masTreshhold {
 
-// 	}
+				sum += 1
+			}
 
-// 	return sum
-// }
+		}
+	}
+
+	fmt.Println("SUM", sum)
+}
 
 func main() {
-	partOneAnswer("sample")
+	partOneAnswer("input")
+	partTwoAnswer("input")
 }
